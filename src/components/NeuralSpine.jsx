895766b3ebe2c8acd1262ef2328ed62f63436e2a
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-20 19:23:39 -0400

    Update NeuralSpine.jsx

--- src/components/NeuralSpine.jsx
+++ src/components/NeuralSpine.jsx
@@ -4,18 +4,8 @@ import React from 'react';
 
 const NeuralSpine = () => {
   return (
-    <div className="relative w-full h-full overflow-hidden pointer-events-none">
-      <div className="absolute inset-0 flex items-center justify-center [perspective:1000px]">
-        <div className="flex flex-col items-center gap-2 [transform-style:preserve-3d] rotate-x-[45deg]">
-          {[...Array(20)].map((_, i) => (
-            <div
-              key={i}
-              className="w-[20px] h-[40px] bg-red-500"
-              style={{ animationDelay: `${i * 0.1}s` }}
-            />
-          ))}
-        </div>
-      </div>
+    <div className="absolute top-0 left-0 z-50 bg-red-500 w-40 h-40">
+      🧬 TEST SPINE
     </div>
   );
 };
