8e7f166f6d1708493eaa46be54ce8e3348f74ec1
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-20 19:28:00 -0400

    Update NeuralSpine.jsx

--- src/components/NeuralSpine.jsx
+++ src/components/NeuralSpine.jsx
@@ -4,8 +4,14 @@ import React from 'react';
 
 const NeuralSpine = () => {
   return (
-    <div className="absolute top-0 left-0 z-50 bg-red-500 w-40 h-40">
-      🧬 TEST SPINE
+    <div className="absolute top-12 left-1/2 -translate-x-1/2 h-[85%] flex flex-col justify-between items-center z-10 pointer-events-none">
+      {[...Array(20)].map((_, i) => (
+        <div
+          key={i}
+          className="w-[10px] h-[20px] rounded-full bg-cyan-400 shadow-[0_0_6px_#00f0ff,0_0_14px_#00f0ff] animate-pulse"
+          style={{ animationDelay: `${i * 0.07}s` }}
+        />
+      ))}
     </div>
   );
 };
