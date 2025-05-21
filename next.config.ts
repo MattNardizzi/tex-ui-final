208d04ba00a088d31dabbf78380965c5797a767c
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-21 09:40:41 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    // GLSL shader support
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["next/babel"],
+            plugins: ["babel-plugin-glsl"],
+          },
+        },
+      ],
+    });
 
-const nextConfig: NextConfig = {
-  /* config options here */
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
