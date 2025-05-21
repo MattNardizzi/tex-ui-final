1b942f6929e8b3f3c96d397b4c1339980672ce83
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-21 07:48:58 -0400

    Update StrategyCoreShell.jsx

--- src/components/ui/StrategyCoreShell.jsx
+++ src/components/ui/StrategyCoreShell.jsx
@@ -1,27 +1,37 @@
 'use client';
 
 import React, { useEffect, useRef } from "react";
-import GazeEyes from "./ui/GazeEyes";
-import TypingPanel from "./ui/TypingPanel";
-import InstitutionalOverlay from "./ui/InstitutionalOverlay";
-import FinanceTicker from "./ui/FinanceTicker";
+
+import GazeEyes from "./GazeEyes";
+import TypingPanel from "./TypingPanel";
+import InstitutionalOverlay from "./InstitutionalOverlay";
+import FinanceTicker from "./FinanceTicker";
+
+import { getNeedPulse } from "../../systems/getNeedPulse";
+import { getCurrentGlowColor, getCurrentEmotionIntensity } from "../../systems/emotionEngine";
 
 export default function StrategyCoreShell() {
   const mount = useRef(null);
 
   useEffect(() => {
-    // Your Three.js + Tone.js scene setup goes here
+    // 🔧 Your Three.js, shader, or Tone.js logic should go here.
+    // This is where your render loop would mount to `mount.current`.
   }, []);
 
   return (
     <div ref={mount} className="relative w-screen h-screen bg-black overflow-hidden">
+      {/* Gaze indicator */}
       <div className="pointer-events-none absolute top-4 left-1/2 -translate-x-1/2">
         <GazeEyes />
       </div>
 
+      {/* Input typing panel */}
       <TypingPanel />
+
+      {/* Central information display */}
       <InstitutionalOverlay />
 
+      {/* Bottom financial ticker */}
       <div className="pointer-events-none absolute bottom-2 w-full flex justify-center">
         <FinanceTicker />
       </div>
