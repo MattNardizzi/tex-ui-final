1fefd74d66ebefa1dc0c02f70e92cb159da035c3
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-20 21:37:12 -0400

    Update emotionEngine.js

--- src/systems/emotionEngine.js
+++ src/systems/emotionEngine.js
@@ -1,33 +1,34 @@
-// 🌌 Cinematic emotion states: grounded, intelligent, emotionally rich
+import * as THREE from 'three';
+
 const emotionStates = {
   calm: {
     name: 'Calm',
-    glowColor: '#00c2ff', // soft electric cyan
+    glowColor: new THREE.Color('#00c2ff'), // soft electric cyan
     pulseRate: 0.5,
   },
   focused: {
     name: 'Focused',
-    glowColor: '#4de2ff', // icy blue
+    glowColor: new THREE.Color('#4de2ff'), // icy blue
     pulseRate: 0.75,
   },
   alert: {
     name: 'Alert',
-    glowColor: '#ffdf40', // warm golden amber
+    glowColor: new THREE.Color('#ffdf40'), // warm golden amber
     pulseRate: 1.0,
   },
   tense: {
     name: 'Tense',
-    glowColor: '#ff6b3b', // reddish-orange ember
+    glowColor: new THREE.Color('#ff6b3b'), // reddish-orange ember
     pulseRate: 1.25,
   },
   overclocked: {
     name: 'Overclocked',
-    glowColor: '#ff0059', // deep pulse magenta
+    glowColor: new THREE.Color('#ff0059'), // deep pulse magenta
     pulseRate: 1.6,
   },
   transcendence: {
     name: 'Transcendence',
-    glowColor: '#9a00ff', // alien violet
+    glowColor: new THREE.Color('#9a00ff'), // alien violet
     pulseRate: 1.2,
   },
 };
@@ -36,8 +37,7 @@ let currentEmotion = 'calm';
 let lastChange = Date.now();
 
 export function getEmotionGlowColor() {
-  const hex = emotionStates[currentEmotion].glowColor;
-  return hex;
+  return emotionStates[currentEmotion].glowColor;
 }
 
 export function getEmotionPulseRate() {
