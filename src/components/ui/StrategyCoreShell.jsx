48de4e1d314d6821e1016792a017383f997333b1
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-21 09:37:47 -0400

    Update StrategyCoreShell.jsx

--- src/components/ui/StrategyCoreShell.jsx
+++ src/components/ui/StrategyCoreShell.jsx
@@ -5,12 +5,15 @@ import TypingPanel from "../TypingPanel"; // Adjusted path to match actual locat
 import MutationOverlay from "./InstitutionalOverlay"; // Now using InstitutionalOverlay as MutationOverlay
 import StatusHUD from "./StatusHUD";
 import ComputationButton from "./ComputationButton";
+import { Canvas } from "@react-three/fiber";
 
 export default function StrategyCoreShell() {
   return (
     <div className="relative w-screen h-screen bg-black overflow-hidden text-white font-mono">
-      {/* Living spine */}
-      <BeamRenderer />
+      {/* 3D Shader Spine */}
+      <Canvas className="absolute inset-0 z-10" camera={{ position: [0, 0, 5], fov: 75 }}>
+        <BeamRenderer />
+      </Canvas>
 
       {/* Top-right status HUD */}
       <StatusHUD />
