c07c393e7aa76cc8441cda612e8cc656999ad290
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-21 09:31:57 -0400

    Update StrategyCoreShell.jsx

--- src/components/ui/StrategyCoreShell.jsx
+++ src/components/ui/StrategyCoreShell.jsx
@@ -1,7 +1,7 @@
 // StrategyCoreShell.jsx
 import React from "react";
 import BeamRenderer from "./BeamRenderer";
-import TypingPanel from "./TypingPanel";
+import TypingPanel from "../TypingPanel"; // Adjusted path to match actual location
 import MutationOverlay from "./MutationOverlay";
 import StatusHUD from "./StatusHUD";
 import ComputationButton from "./ComputationButton";
@@ -9,10 +9,19 @@ import ComputationButton from "./ComputationButton";
 export default function StrategyCoreShell() {
   return (
     <div className="relative w-screen h-screen bg-black overflow-hidden text-white font-mono">
+      {/* Living spine */}
       <BeamRenderer />
+
+      {/* Top-right status HUD */}
       <StatusHUD />
+
+      {/* Top-left control button */}
       <ComputationButton />
+
+      {/* Output feed from Tex */}
       <TypingPanel />
+
+      {/* Subtle glitch effect overlay */}
       <MutationOverlay />
     </div>
   );
